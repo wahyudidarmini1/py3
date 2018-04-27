957540a476dc8b783b4c9e7aa9d2efadafc3ac85
wahyudidarmini1 <37975743+wahyudidarmini1@users.noreply.github.com>
2018-04-27 15:07:23 +0700

    Update channel.py

--- LINEPY/lib/channel.py
+++ LINEPY/lib/channel.py
@@ -18,8 +18,8 @@ class Channel:
     http_query_path = "/S4"
     channel_query_path = "/CH4"
 
-    UA = "Line/5.5.1.1.590"
-    LA = "DESKTOPWIN\t5.5.1.1.1590\tWINDOWS_NT\t8.0"
+    UA = "Line/7.14.0"
+    LA = "IOSIPAD\t7.14.0\tiPhone OS\t10.12.0"
 
     authToken = None
     mid = None
